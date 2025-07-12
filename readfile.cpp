f4249c0eea76abcd3a13e7c70533708ce8bedf02
Prinshav Kandel <kandelprinshav1@gmail.com>
2025-07-12 12:21:59 +0545

    Update readfile.cpp
    two things left:
    create plot values for the PDF using vector,
    create a link that links the frontend to the backend

--- readfile.cpp
+++ readfile.cpp
@@ -3,9 +3,20 @@
 #include <string>
 #include <windows.h>
 #include <commdlg.h>
+#include<algorithm>
+#include<unordered_map>
+#include <cmath>
 
 using namespace std;
-
+// structure definition
+struct tstats{
+    double mean;
+    double median;
+    double mode;
+    double std_dev;
+    double variance;
+    double iqr;
+};
 // Bubble sort function 
 void bubbleSort(double arr[], int n) {
     for (int i = 0; i < n-1; i++) {
@@ -21,7 +32,54 @@ void bubbleSort(double arr[], int n) {
         if (!swapped) break;
     }
 }
+void calculateStats(double arr[],int n, tstats& s)
+{
+ bubbleSort(arr,n);
+     // Mean
+    double sum = 0;
+    for (int i = 0; i < n; i++) sum += arr[i];
+    s.mean = sum / n;
 
+    // Median
+    if (n % 2 == 0)
+        s.median = (arr[n / 2 - 1] + arr[n / 2]) / 2.0;
+    else
+        s.median = arr[n / 2];
+
+    // Mode
+    unordered_map<double, int> freq;
+    int maxFreq = 0;
+    s.mode = arr[0];
+    for (int i = 0; i < n; i++) {
+        freq[arr[i]]++;
+        if (freq[arr[i]] > maxFreq) {
+            maxFreq = freq[arr[i]];
+            s.mode = arr[i];
+        }
+    }
+
+    // Variance and Standard Deviation
+    double var_sum = 0;
+    for (int i = 0; i < n; i++) {
+        var_sum += (arr[i] - s.mean) * (arr[i] - s.mean);
+    }
+    s.variance = var_sum / n;
+    s.std_dev = sqrt(s.variance);
+
+    // Interquartile Range (IQR)
+    double q1, q3;
+    int mid = n / 2;
+    if (n % 2 == 0) {
+        // Even
+        q1 = (arr[mid / 2 - 1] + arr[mid / 2]) / 2.0;
+        q3 = (arr[mid + mid / 2 - 1] + arr[mid + mid / 2]) / 2.0;
+    } else {
+        // Odd
+        q1 = arr[mid / 2];
+        q3 = arr[mid + mid / 2 + 1];
+    }
+    s.iqr = q3 - q1;
+}
 int main() {
     const int Max_size = 1000;
     double data[Max_size];
@@ -62,19 +120,16 @@ int main() {
     file.close();
 
     cout << "\nTotal numbers read: " << count << endl;
-
-    cout << "\nOriginal numbers:\n";
-    for (int i = 0; i < count; i++) {
-        cout << data[i] << " ";
-    }
-
-    bubbleSort(data, count);
-
-    cout << "\n\nSorted numbers:\n";
-    for (int i = 0; i < count; i++) {
-        cout << data[i] << " ";
-    }
-    cout << endl;
+   
+   
+    tstats s;
+    calculateStats(data,count,s);
+    cout << "Mean: " << s.mean << endl;
+    cout << "Median: " << s.median << endl;
+    cout << "Mode: " << s.mode << endl;
+    cout << "Standard Deviation: " << s.std_dev << endl;
+    cout << "Variance: " << s.variance << endl;
+    cout << "IQR: " << s.iqr << endl;
 
     return 0;
 }
